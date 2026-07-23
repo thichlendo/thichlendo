acb911dbbec114ea7185b0f67b89262b6493616a
thichlendo <thichlendo139@gmail.com>
2026-07-23 14:53:15 +0700

    Refactor Home component in page.tsx
    Removed unused import and cleaned up the component structure.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "sans-serif" }}>
       <h1>Chào mừng đến với trang web của tôi!</h1>
@@ -11,4 +6,3 @@ export default function Home() {
     </div>
   );
 }
-
