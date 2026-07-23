0cc360d1e243851e1eb41ae23e1ced61684d90a9
thichlendo <thichlendo139@gmail.com>
2026-07-23 16:08:44 +0700

    Remove Home component from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "sans-serif" }}>
-      <h1>Chào mừng đến với trang web của tôi!</h1>
-      <p>Tôi đang tự tay code web đây.</p>
-    </div>
-  );
-}
+
